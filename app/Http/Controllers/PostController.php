597a947b917319751eb7364d19bd074943e9b650
SalarCloudways <salar.ali@cloudways>
2023-05-19 10:22:44 +0500

    changs done as communicated

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
-use App\Http\Resources\PostsResource;
+use App\Http\Resources\PostResource;
 use App\Repositories\Interfaces\PostInterface;
 
 class PostController extends Controller
@@ -22,27 +22,27 @@ class PostController extends Controller
     public function allposts(){
         
         $allPosts = $this->postsRepository->allposts();
-        return Response(PostsResource::collection($allPosts), 200);
+        return Response(PostResource::collection($allPosts), 200);
     }
 
     //Show All Posts By Same Author
     public function allPostsByAuthor($authorID){
 
         $postByAuthor = $this->postsRepository->allPostsByAuthor($authorID);
-        return Response(PostsResource::collection($postByAuthor), 200);
+        return Response(PostResource::collection($postByAuthor), 200);
     }
 
     //Get Single Post By ID
     public function postById(Post $post){
 
-        return Response(new PostsResource($post), 200);
+        return Response(new PostResource($post), 200);
     }
 
     //Create Post
     public function createPost(PostRequest $request){
 
         $createPost = $this->postsRepository->createPost($request);
-        return Response(new PostsResource($createPost), 200);
+        return Response(new PostResource($createPost), 200);
     }
 
     //Delete Post
@@ -56,6 +56,6 @@ class PostController extends Controller
     public function updatePost(Post $post, PostRequest $request){
 
         $updatePost = $this->postsRepository->updatePost($post, $request);
-        return Response(new PostsResource($updatePost), 200);
+        return Response(new PostResource($updatePost), 200);
     }
 }
